192626895dff94da52660f203c3fcdee035032b6
Usuario de ejemplo <tiagotroller021019@gmail.com>
2026-04-04 16:25:07 -0500

    feat: Correccion de errores

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const initializeDatabase = require('../config/initDB');
-require('dotenv').config();
 
 const paymentRoutes = require('../routes/payment');
 const orderRoutes = require('../routes/orders');
